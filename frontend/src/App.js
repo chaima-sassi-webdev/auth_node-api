cf8d39470034ecec89edbc5f4bf6787626ffb014
chaima <chaima@VM-CLOUD.myguest.virtualbox.org>
2025-06-25 09:36:55 +0200

    fix push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Login from "./login";
+import Register from "./signup";
+import ForgotPassword from "./forgetPassword"; 
+import UsersList from "./users";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>Accueil</div>} />
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forget-password" element={<ForgotPassword /> } />
+      <Route path="/users" element={<UsersList /> } />
     </Routes>
   );
 }
