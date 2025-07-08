fe55e3f1676562528464a747a18bf301a83ed69d
chaima <chaima@VM-CLOUD.myguest.virtualbox.org>
2025-07-08 05:38:28 +0200

    vercel config

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -19,7 +19,8 @@ function Login() {
       return;
     }
     try {
-      const response = await fetch("http://localhost:4000/api/auth/login", {
+     // const response = await fetch("http://localhost:4000/api/auth/login", {
+       const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
